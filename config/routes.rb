345405489fa66706d0d580ceaea4a08524abe627
Nate Su <enpingsu555@gmail.com>
2020-08-06 11:37:03 +0800

    fixed comma

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      post "users" , to: "users#login", as: "login"
+      post "users", to: "users#login", as: "login"
       resources :users, only: [:update]
       resources :rentals, only: [:index, :show, :create, :update, :destroy] do
         resources :orders, only: [:create]
